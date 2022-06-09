fcc8b11a5cb31efc76273de60779e142e9bd5214
DannnLim <Dannylim-@hotmail.com>
2022-06-09 18:16:11 +1000

    commit tree ae3479591273011f85220cf3b85d8eb1a03e87dd

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+    if (divide = 2000 / 100) {
+        return 20;
+    }
+}
+
+let square = x => 2 * 2
+
+let a = 3;
+let b = 4;
+function add(a, b) {
+    if (a => b) {
+        return a + b;
+    }
+}
